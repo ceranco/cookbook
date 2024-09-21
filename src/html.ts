7fe0b6038fe6bbcaa2039ac0b25afce5d0f2bccc
Eran Cohen <ceranco@gmail.com>
2024-09-21 20:32:34 +0000

    Switch encoder to use HTML objects.

--- src/html.ts
+++ src/html.ts
@@ -1,33 +1,71 @@
 export abstract class Html {
-	static fieldset(class_: string, legend: string, ...content: string[]): string {
-		return `<fieldset class=${class_}><legend>${legend}</legend>${content.join("")}</fieldset>`;
+	static fieldset(class_: string, legend: string, ...elements: Node[]): HTMLFieldSetElement {
+		let legendElement = document.createElement("legend");
+		legendElement.textContent = legend;
+
+		let fieldset = document.createElement("fieldset");
+		fieldset.className = class_;
+		fieldset.appendChild(legendElement);
+		elements.forEach(child => fieldset.appendChild(child));
+
+		return fieldset;
 	}
 
-	static li(content: string): string {
-		return `<li>${content}</li>`;
+	static li(content: Node): HTMLLIElement {
+		let li = document.createElement("li");
+		li.appendChild(content);
+
+		return li;
 	}
 
-	static ul(class_: string, content: string): string {
-		return `<ul class="${class_}">${content}</ul>`;
+	static ul(class_: string, items: HTMLLIElement[]): HTMLUListElement {
+		let ul = document.createElement("ul");
+		ul.className = class_;
+		items.forEach(item => ul.appendChild(item));
+
+		return ul;
 	}
 
-	static label(for_: string, class_: string, text: string): string {
-		return `<label for="${for_}" class="${class_}">${text}</label>`;
+	static label(for_: string, class_: string, text: string): HTMLLabelElement {
+		let label = document.createElement("label");
+		label.htmlFor = for_;
+		label.className = class_;
+		label.textContent = text;
+
+		return label;
 	}
 
-	static input(name: string, value: string): string {
-		return `<input type="text" name="${name}" value="${value}" required />`;
+	static input(name: string, value: string): HTMLInputElement {
+		let input = document.createElement("input");
+		input.type = "text";
+		input.name = name;
+		input.value = value;
+		input.required = true;
+
+		return input;
 	}
 
-	static button(type: string, class_: string, text: string): string {
-		return `<button type="${type}" class="${class_}">${text}</button>`;
+	static button(type: "button" | "submit" | "reset", class_: string, text: string): HTMLButtonElement {
+		let button = document.createElement("button");
+		button.type = type;
+		button.className = class_;
+		button.textContent = text;
+
+		return button;
 	}
 
-	static div(...content: string[]) {
-		return `<div>${content.join("")}</div>`;
+	static div(...elements: Node[]): HTMLDivElement {
+		let div = document.createElement("div");
+		elements.forEach(child => div.appendChild(child));
+
+		return div;
 	}
 
-	static form(class_: string, ...content: string[]): string {
-		return `<form class=${class_}>${content.join("")}</form>`;
+	static form(class_: string, ...children: Node[]): HTMLFormElement {
+		let form = document.createElement("form");
+		form.className = class_;
+		children.forEach(child => form.appendChild(child));
+
+		return form;
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,55 +2,54 @@ import { Html } from "./html";
 import { Ingredient, IngredientList, Recipe, Section, SectionList, Step, StepList } from "./recipe";
 
 abstract class HtmlFormFormatter {
-	static formatIngredient(ingredient: Ingredient): string {
+	static formatIngredient(ingredient: Ingredient): HTMLLIElement {
 		return Html.li(Html.input("ingredient", ingredient));
 	}
 
-	static formatIngredientList(ingredients: IngredientList): string {
+	static formatIngredientList(ingredients: IngredientList): HTMLElement {
 		const button = Html.button("button", "add_ingredient", "הוסף רכיב");
 
 		let list = ingredients.map(HtmlFormFormatter.formatIngredient);
 		list.push(Html.li(button));
 
-		const htmlIngredients = Html.ul("recipe_ingredients", list.join(""));
+		const htmlIngredients = Html.ul("recipe_ingredients", list);
 
 		return Html.fieldset("ingredients", "רכיבים", htmlIngredients);
 	}
 
-	static formatStep(step: Step): string {
+	static formatStep(step: Step): HTMLLIElement {
 		return Html.li(Html.input("step", step));
 	}
 
-	static formatStepList(steps: StepList): string {
+	static formatStepList(steps: StepList): HTMLElement {
 		const button = Html.button("button", "add_step", "הוסף שלב");
 
 		let list = steps.map(HtmlFormFormatter.formatStep);
 		list.push(Html.li(button));
 
-		const htmlSteps = Html.ul("recipe_steps", list.join(""));
+		const htmlSteps = Html.ul("recipe_steps", list);
 
 		return Html.fieldset("steps", "הוראות הכנה", htmlSteps);
 	}
 
-	static formatSection(section: Section): string {
+	static formatSection(section: Section): HTMLLIElement {
 		let name = Html.div(Html.label("section_name", "section_name", "שם שלב:"), Html.input("section_name", section.name));
 		let ingredients = HtmlFormFormatter.formatIngredientList(section.ingredients);
 		let steps = HtmlFormFormatter.formatStepList(section.steps);
 
-
 		return Html.li(Html.fieldset("section", "שלב", name, ingredients, steps));
 	}
 
-	static formatSectionList(sections: SectionList): string {
+	static formatSectionList(sections: SectionList): HTMLElement {
 		const button = Html.button("button", "add_section", "הוסף שלב");
 
 		let list = sections.map(HtmlFormFormatter.formatSection);
 		list.push(Html.li(button));
 
-		return Html.ul("recipe_sections", list.join(""));
+		return Html.ul("recipe_sections", list);
 	}
 
-	static formatRecipe(recipe: Recipe): string {
+	static formatRecipe(recipe: Recipe): HTMLElement {
 		let nameLabel = Html.label("recipe_name", "recipe_name", "שם מתכון:");
 		let nameInput = Html.input("recipe_name", recipe.name);
 		let sections = HtmlFormFormatter.formatSectionList(recipe.sections);
@@ -100,5 +99,5 @@ const recipe = new Recipe("פיצה", [
 // let recipe = Recipe.empty();
 const html = HtmlFormFormatter.formatRecipe(recipe);
 let app = document.body.querySelector("#app")!;
-app.innerHTML = html
+app.replaceChildren(html);
 console.log(HtmlFormDecoder.decodeRecipe(app));
